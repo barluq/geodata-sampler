c19f02cd3099719fa6d6e0a013d214e929635e3d
Lukas Bares <barluq697@gmail.com>
2025-07-31 10:07:42 +0000

    sample geojson works

--- core/shared/formats_geojson.py
+++ core/shared/formats_geojson.py
@@ -1,5 +1,6 @@
 
-from core.shared.spatial_randoms import random_point_coordinates, random_properties_set
+from typing import Generator, Literal
+from core.shared.spatial_randoms import random_point_coordinates, random_properties_set, random_properties_set_per_year
 
 
 def geojson_feature_collection_body(features: list) -> dict:
@@ -15,7 +16,11 @@ def geojson_feature_collection_body(features: list) -> dict:
     }
 
 
-def geojson_features_generator(number_of_features: int = 1):
+def geojson_features_generator(
+        number_of_features: int, 
+        attributes: dict[str, Literal["string", "integer", "float"]],
+        year_range: tuple[int, int] = (2000, 2023)
+        ) -> Generator[dict, None, None]:
 
     def geojson_point_geometry(coordinates: tuple[float, float]) -> dict:
         return {
@@ -24,13 +29,18 @@ def geojson_features_generator(number_of_features: int = 1):
         }
 
     def geojson_properties(fid: int) -> dict:
-        return {
-            "fid": fid,
-            **random_properties_set(year_range=(2000, 2023)),
-        }
+        if year_range is None:
+            return {
+                "fid": fid,
+                **random_properties_set(attributes=attributes),
+            }
+        else:
+            return {
+                "fid": fid,
+                **random_properties_set_per_year(year_range=year_range, attributes=attributes),
+            }
 
     for i in range(number_of_features):
-
         yield {
             "type": "Feature",
             "geometry": geojson_point_geometry(random_point_coordinates()),
--- core/shared/spatial_randoms.py
+++ core/shared/spatial_randoms.py
@@ -1,44 +1,38 @@
 import random
+from typing import Literal
 
-
-_random_urban_spatial_attributes: dict[str, str | int | float] = {
-    "population_density": "integer",
-    "land_use": "string",
-    "building_height": "float",
-    "green_space_ratio": "float",
-    "transportation_accessibility": "string",
-    "urban_infrastructure_quality": "string",
-    "socioeconomic_status": "string",
-    "crime_rate": "float",
-    "public_services_availability": "string",
-    "cultural_attractions": "string",
-    "environmental_quality": "string",
-    "urban_heat_island_effect": "float",
-    "walkability_score": "float",
-    "bicycle_infrastructure": "string",
-    "public_transportation_quality": "string",
-    "urban_greening_initiatives": "string", 
-}
-
-_year_range: tuple[int, int] = (2000, 2023)
-
+from shared.value_randoms import _random_float, _random_integer, _random_string
 
 def random_point_coordinates() -> tuple[float, float]:
     """Generate random coordinates for a point."""
     return (random.uniform(-180, 180), random.uniform(-90, 90))
 
 
-def random_properties_set(year_range: tuple[int, int]) -> dict[str, int | float | str]:
+def random_properties_set(attributes: dict[str, Literal["string", "integer", "float"]]) -> dict[str, int | float | str]:
+    result: dict = {}
+    for key, value in attributes.items():
+        if value == "integer":
+            result[key] = _random_integer()
+        elif value == "float":
+            result[key] = _random_float()
+        elif value == "string":
+            result[key] = _random_string()
+        else:
+            result[key] = "unknown"
+    return result
+
+
+def random_properties_set_per_year(year_range: tuple[int, int], attributes: dict[str, Literal["string", "integer", "float"]]) -> dict[str, int | float | str]:
     result: dict = {}
     for year in range(year_range[0], year_range[1] + 1):
-        for key, value in _random_urban_spatial_attributes.items():
+        for key, value in attributes.items():
             year_key = f"{key}_{year}"
             if value == "integer":
-                result[year_key] = random.randint(1, 100)
+                result[year_key] = _random_integer()
             elif value == "float":
-                result[year_key] = random.uniform(0.0, 100.0)
+                result[year_key] = _random_float()
             elif value == "string":
-                result[year_key] = random.choice(["low", "medium", "high", "very high"])
+                result[year_key] = _random_string()
             else:
                 result[year_key] = "unknown"
     return result

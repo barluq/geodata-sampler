7d98cdcdd6390c75a98020306beae3fb6359b78a
Lukas Bares <barluq697@gmail.com>
2025-07-31 15:54:42 +0000

    just timeseries for now

--- core/shared/io_store_data.py
+++ core/shared/io_store_data.py
@@ -3,13 +3,15 @@ from pathlib import Path
 
 def store_geojson_to_generated_folder(
     geojson_data: dict,
+    folder_path: Path,
     filename: str,
 ) -> None:
     """
-    Stores the provided GeoJSON data into a file within the '../../data/generated' directory.
+    Stores the provided GeoJSON data into a file within the specified folder path.
 
     Args:
         geojson_data (dict): The GeoJSON data to be saved.
+        folder_path (Path): The folder path where the file will be saved.
         filename (str): The name of the file to save the data as.
 
     Returns:
@@ -18,10 +20,9 @@ def store_geojson_to_generated_folder(
     The function ensures that the output directory exists before writing the GeoJSON data to the specified file.
     """
     # Ensure the output directory exists
-    output_dir = Path('../../data/generated')
-    output_dir.mkdir(parents=True, exist_ok=True)
+    folder_path.mkdir(parents=True, exist_ok=True)
 
     # Write the GeoJSON to a file
-    output_path = output_dir / filename
+    output_path = folder_path / filename
     with open(output_path, 'w') as f:
         json.dump(geojson_data, f, indent=2)
